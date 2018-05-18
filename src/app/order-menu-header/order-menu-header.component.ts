fa884825cbe144c261a54d01d9be7c376988c582
tomshudev <tomshudev@gmail.com>
2018-05-18 14:31:53 +0300

    Creating the header Canceling the fixed of the details and category chooser in order to display the header properly

---
+++ src/app/order-menu-header/order-menu-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-menu-header',
+  templateUrl: './order-menu-header.component.html',
+  styleUrls: ['./order-menu-header.component.scss']
+})
+export class OrderMenuHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
